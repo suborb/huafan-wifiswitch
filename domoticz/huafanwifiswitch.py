aa6b23a8d3c7a69827b8d810977aa9615043ec28
suborb <dom@suborbital.org.uk>
2016-12-20 15:21:17 +0000

    Initial cut at a domoticz python plugin for the switch.
    Turns on/off but:
    
    - Device types are incorrect
    - Doesn't report back the usage (polls for it on each heartbeat though)

---
+++ domoticz/huafanwifiswitch.py
@@ -0,0 +1,105 @@
+"""
+<plugin key="huafanwifiswitch" name="HuaFan Wifi Switch" author="suborb" version="1.0.0" wikilink="http://github.com/suborb/huafan-wifiswitch" externallink="http://localhost/">
+    <params>
+        <param field="Address" label="IP Address" width="200px" required="true" default="127.0.0.1"/>
+    </params>
+</plugin>
+
+"""
+import sys
+sys.path.append("/usr/local/lib/python3.4/dist-packages/")
+import Domoticz
+from Crypto.Cipher import AES
+import base64
+
+def onStart():
+    # TODO: Work out the right value for these switches, they are wrong
+    if (len(Devices) == 0):
+        Domoticz.Device(Name="Status",  Unit=1, Type=244,  Switchtype=73).Create()
+        Domoticz.Device(Name="Usage", Unit=2, Type=243, Subtype=29, Switchtype=0, Image=0, Options="").Create()
+
+    Domoticz.Transport("TCP/IP", Parameters["Address"], "7681")
+    Domoticz.Protocol("line")
+    Domoticz.Connect()
+    return True
+
+def onConnect(Status, Description):
+    if (Status == 0):
+        print("Connected")
+
+    return True
+
+def onMessage(Data):
+    decrypted = decrypt(Data, "9521314528002574")
+    decrypted = str(decrypted,'utf-8')
+    parts = decrypted.split()
+
+    if parts[0] == 'S':
+        if parts[2].startswith("+SNODE") == True:
+            index = parts[2].index(",")
+            result = parts[2][index+1]
+            if result == '1':
+                UpdateDevice(1, 1, '')
+            if result == '2':
+                UpdateDevice(1, 0, '')
+        else:
+            print(decrypted)
+    return True
+
+def onCommand(Unit, Command, Level, Hue):
+    Command = Command.strip()
+    action, sep, params = Command.partition(' ')
+    action = action.capitalize()
+
+    if action == 'On':
+        execCommand("AT+SOPEN=1")
+    if action == 'Off':
+        execCommand("AT+SCLOSE=1")
+
+   
+    return True
+
+def onNotification(Data):
+    Domoticz.Log("Notification: " + str(Data))
+    return
+
+def onHeartbeat():
+    print("Heartbeat")
+    execCommand("AT+SCURRENTPOWER=1")
+    return True
+
+def onDisconnect():
+    print("Disconnected")
+    return
+   
+
+def onStop():
+    Domoticz.Log("onStop called")
+    return True
+
+
+def encrypt(message, passphrase):
+    IV = "4528453102574529"
+    length = 16 - (len(message) % 16)
+    message += chr(length)*length
+    aes = AES.new(passphrase, AES.MODE_CBC, IV)
+    return base64.b64encode(aes.encrypt(message))
+
+def decrypt(encrypted, passphrase):
+    IV = "4528453102574529"
+    aes = AES.new(passphrase, AES.MODE_CBC, IV)
+    return aes.decrypt(base64.b64decode(encrypted))
+
+def execCommand(cmd):
+    msg="P 888888 " + cmd + "\r\n"
+    tosend=str(encrypt(msg,"9521314528002574"),'utf-8')
+    Domoticz.Send(tosend)
+    return
+
+def UpdateDevice(Unit, nValue, sValue):
+    # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
+    if (Unit in Devices):
+        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
+            Devices[Unit].Update(nValue, str(sValue))
+            Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
+    return
